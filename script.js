ebdf1eff3f5e072a0251d2d3cc755274482a7767
Your Name <your-email@gmail.com>
2026-04-28 12:50:12 +0800

    display user and computer choices

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ function play(userChoice) {
 const choices = ["rock", "paper", "scissors"];
 const computerChoice = choices[Math.floor(Math.random() * 3)];
 
+document.getElementById("user-choice").innerText =
+    "Your Choice: " + userChoice;
+
+document.getElementById("computer-choice").innerText =
+    "Computer Choice: " + computerChoice;
+
 }
