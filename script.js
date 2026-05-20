8200cc5ad01313805eba3629dbad6e76f7ff597a
Your Name <your-email@gmail.com>
2026-05-20 09:30:17 +0800

    i added the google font link

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
 function play(userChoice) {
-    
-const choices = ["rock", "paper", "scissors"];
-const computerChoice = choices[Math.floor(Math.random() * 3)];
 
-document.getElementById("user-choice").innerText =
-    "Your Choice: " + userChoice;
+    const choices = ["rock", "paper", "scissors"];
 
-document.getElementById("computer-choice").innerText =
-    "Computer Choice: " + computerChoice;
+    const computerChoice =
+        choices[Math.floor(Math.random() * 3)];
+
+    document.getElementById("user-choice").innerText =
+        "Your Choice: " + userChoice;
+
+    document.getElementById("computer-choice").innerText =
+        "Computer Choice: " + computerChoice;
+
     let result = "";
 
-if (userChoice === computerChoice) {
-    result = "It's a Draw!";
-} 
-else if (
-    (userChoice === "rock" && computerChoice === "scissors") ||
-    (userChoice === "paper" && computerChoice === "rock") ||
-    (userChoice === "scissors" && computerChoice === "paper")
-) {
-    result = "You Win! 🎉";
-} 
-else {
-    result = "You Lose! 😢";
-}
-document.getElementById("winner").innerText = result;
+    if (userChoice === computerChoice) {
+
+        result = "It's a Draw!";
+
+    }
+
+    else if (
+
+        (userChoice === "rock" && computerChoice === "scissors") ||
+
+        (userChoice === "paper" && computerChoice === "rock") ||
+
+        (userChoice === "scissors" && computerChoice === "paper")
+
+    ) {
+
+        result = "You Win! 🎉";
+
+    }
+
+    else {
+
+        result = "You Lose! 😢";
+
+    }
+
+    document.getElementById("winner").innerText = result;
 
 }
+
